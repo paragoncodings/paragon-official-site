a954499e733d8024067249a5622faca8624b95b9
paragoncodings <paragoncodings@gmail.com>
2024-10-27 14:48:22 +0500

    some changes

--- paragoncodings/src/App.jsx
+++ paragoncodings/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import './App.css'
+import React, { Fragment } from 'react'
+
 
 function App() {
  
 
   return (
-    <>
-  <h1>This is Paragon Codings official site</h1>
-    </>
+    <Fragment>
+<div className='bg-[#37383C] w-full h-lvh'>
+<h1 className='text-2xl text-center text-[#73FAF9]'>this is our website we start@29sep</h1>
+
+</div>
+    </Fragment>
   )
 }
 
