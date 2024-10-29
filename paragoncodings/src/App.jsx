ffb35b7128aa3e89b8e86dd7245f1b66843f4ff8
paragoncodings <paragoncodings@gmail.com>
2024-10-29 22:08:10 +0500

    bug

--- paragoncodings/src/App.jsx
+++ paragoncodings/src/App.jsx
@@ -1,62 +1,38 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ScrollToTop from "react-scroll-to-top";
-import { FaArrowUp } from 'react-icons/fa'; 
-import Home from './Component/Pages/home'
-import NavBar from './Component/Navbar/Navbar';
+import { FaArrowUp } from "react-icons/fa";
+import NavBar from "./Component/Navbar/NavBar";
 
-import Home from './Component/Pages/home';
-import ScrollToTop from "react-scroll-to-top";
-import { FaArrowUp } from 'react-icons/fa'; 
-// import Home from './Component/Pages/Home'
+import Home from "./Component/Pages/Home";
 
 function App() {
- 
-
   return (
     <Fragment>
-       <Router>
-        <NavBar/>
-      <Routes>
-         <Route path="/" element={<Home />} />
-       
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
 
-    
-     
-
-      </Routes>
-    </Router>
-
-    <ScrollToTop
-
- <ScrollToTop
-
+      <ScrollToTop
         smooth
-        component={<FaArrowUp />} 
+        component={<FaArrowUp />}
         style={{
           backgroundColor: "#C32735",
           color: "white",
           borderRadius: "50%",
           padding: "10px",
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", 
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
           border: "2px solid white",
           transition: "transform 0.3s ease",
         }}
-        className="hover:scale-110" 
+        className="hover:scale-110"
       />
-
-
-
-
     </Fragment>
-  )
+  );
 }
 
-export default App
-
-
-
-
-
-
+export default App;
